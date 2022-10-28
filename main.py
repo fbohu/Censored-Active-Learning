e62cc17424eaa6e621abda0edae66f534d0ed44e
Frederik Boe Huttel <fbohy@dtu.dk>
2022-10-28 16:43:28 +0200

    support

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def get_strat(which):
 def get_model(which, x_train):
     return {'small': {'in_features': x_train.shape[-1],
                     'out_features': 4,
-                    #'hidden_size':[128,128],
-                    'hidden_size':[32,32],
+                    'hidden_size':[128,128],
+                    #'hidden_size':[16, 16],
                     'dropout_p': 0.25,
                     'epochs': 1000,
                     'lr_rate':3e-4,
